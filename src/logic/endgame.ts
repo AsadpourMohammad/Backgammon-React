91a22790367524d476e704f1d6f5dc3da2fe5012
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-01-21 03:56:37 +0330

    endgame.js rewrote to Typescript

--- src/logic/endgame.ts
+++ src/logic/endgame.ts
@@ -1,46 +1,44 @@
 import { toast } from "react-hot-toast";
 import { toastStyle } from "../App";
+import Player from "./player";
 
-export function readyToEnd(board, turn) {
-  const containing = [];
+export function readyToEnd(board: string[], turn: Player) {
+  const containing: number[] = [];
+
   board.map((bar, barIdx) => {
     if (bar.includes(turn.player)) containing.push(barIdx);
   });
 
   if (turn.player === "White") {
-    // for (let i = 0; i < containing.length; i++) {
-    //   const barIdx = containing[i];
+    for (let i = 0; i < containing.length; i++) {
+      const barIdx = containing[i];
 
-    //   if (barIdx < 6 || barIdx > 11) {
-    //     return false;
-    //   }
-    // }
-
-    containing.map((barIdx) => {
-      if (barIdx < 6 || barIdx > 11) return false;
-    });
+      if (barIdx < 6 || barIdx > 11) {
+        return false;
+      }
+    }
   } else {
-    // for (let i = 0; i < containing.length; i++) {
-    //   const barIdx = containing[i];
+    for (let i = 0; i < containing.length; i++) {
+      const barIdx = containing[i];
 
-    //   if (barIdx < 18) {
-    //     return false;
-    //   }
-    // }
-
-    containing.map((barIdx) => {
-      if (barIdx < 18) return false;
-    });
+      if (barIdx < 18) {
+        return false;
+      }
+    }
   }
 
   return true;
 }
 
-export function calcEndingDiceBars(board, turn, dices) {
-  const canGoFrom = [];
+export function calcEndingDiceBars(
+  board: string[],
+  turn: Player,
+  dices: number[]
+) {
+  const canGoFrom: number[] = [];
   const [firstDice, secondDice] = dices;
 
-  if (turn === "White") {
+  if (turn.player === "White") {
     if (firstDice > 0 && board[24 - firstDice].includes(turn.player)) {
       canGoFrom.push(24 - firstDice);
     }
@@ -68,7 +66,7 @@ export function calcEndingDiceBars(board, turn, dices) {
   return canGoFrom;
 }
 
-export function celebrateGameEnd(turn) {
+export function celebrateGameEnd(turn: Player) {
   toast(
     turn.player === "White"
       ? `⚪ WHITE ⚪ has Won the Game!`
